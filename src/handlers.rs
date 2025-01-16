72a5b8e77c13375eb77dda688d7f068fee0a8a42
ryomaejii <ryo.maejii@gmail.com>
2025-01-16 22:30:51 +0900

    feat: Implement update functionality for Todo items and add corresponding test case

--- src/handlers.rs
+++ src/handlers.rs
@@ -39,9 +39,11 @@ pub async fn update_todo<T: TodoRepository>(
     Json(payload): Json<UpdateTodo>,
     Extension(repository): Extension<Arc<T>>,
 ) -> Result<impl IntoResponse, StatusCode> {
-    todo!();
+    let todo = repository
+        .update(id, payload)
+        .or(Err(StatusCode::NOT_FOUND))?;
 
-    Ok(StatusCode::OK)
+    Ok((StatusCode::CREATED, Json(todo)))
 }
 
 pub async fn delete_todo<T: TodoRepository>(
